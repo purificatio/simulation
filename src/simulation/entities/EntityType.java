f7e577ae3da0f65924cb8f4918646e5f10e72b6e
Kyryl Shylov <shylov.work@gmail.com>
2025-10-25 19:52:41 +0300

    Refactor EntityType (no longer has entity passability param) Add Notes

--- src/simulation/entities/EntityType.java
+++ src/simulation/entities/EntityType.java
@@ -1,18 +1,10 @@
 package simulation.entities;
 
 public enum EntityType {
-    HERBIVORE(false),
-    PREDATOR(false),
-    ROCK(false),
-    TREE(false),
-    GRASS(false),
-    NOTHING(true);
-    private final boolean passability;
-    EntityType(boolean passability){
-        this.passability = passability;
-    }
-
-    public boolean isPassability() {
-        return passability;
-    }
+    HERBIVORE,
+    PREDATOR,
+    ROCK,
+    TREE,
+    GRASS,
+    NOTHING;
 }
